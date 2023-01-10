a360ff93f9ad9d0b4f3b97310fd596ac1325be11
ghostclog <kdy11281@gmail.com>
2023-01-10 15:54:15 +0900

    데이터 베이스 구조 변경(3번째)

--- the_project/web_back/models.py
+++ the_project/web_back/models.py
@@ -2,36 +2,50 @@ from django.db import models
 
 # Create your models here.
 
-class FindPeople(models.Model):
-    post_code = models.AutoField(primary_key=True)
+class CommentData(models.Model):
+    comment_id = models.IntegerField(primary_key=True)
+    comment_cont = models.CharField(max_length=1024)
     user = models.ForeignKey('UserData', models.DO_NOTHING)
-    team_name = models.ForeignKey('TeamData', models.DO_NOTHING, db_column='team_name')
-    post_title = models.CharField(max_length=128)
-    post_con = models.TextField()
+    post = models.ForeignKey('PostData', models.DO_NOTHING)
+    comment_time = models.DateTimeField()
 
     class Meta:
         managed = False
-        db_table = 'find_people'
-        unique_together = (('post_code', 'user'),)
+        db_table = 'comment_data'
 
 
-class RegitData(models.Model):
-    res_code = models.AutoField(primary_key=True)
+class PostData(models.Model):
+    post_id = models.IntegerField(primary_key=True)
+    category = models.CharField(max_length=45)
     user = models.ForeignKey('UserData', models.DO_NOTHING)
-    team_name = models.ForeignKey('TeamData', models.DO_NOTHING, db_column='team_name')
+    contents_data = models.TextField(blank=True, null=True)
+    post_time = models.DateTimeField(blank=True, null=True)
 
     class Meta:
         managed = False
-        db_table = 'regit_data'
+        db_table = 'post_data'
 
 
 class TeamData(models.Model):
     team_name = models.CharField(primary_key=True, max_length=250)
     user = models.ForeignKey('UserData', models.DO_NOTHING)
+    introduction = models.CharField(max_length=1024, blank=True, null=True)
+    team_make_time = models.DateTimeField()
 
     class Meta:
         managed = False
         db_table = 'team_data'
+
+
+class TeamUserData(models.Model):
+    user = models.OneToOneField('UserData', models.DO_NOTHING, primary_key=True)
+    tema_name = models.ForeignKey(TeamData, models.DO_NOTHING, db_column='tema_name')
+    is_admin = models.CharField(max_length=1)
+
+    class Meta:
+        managed = False
+        db_table = 'team_user_data'
+        unique_together = (('user', 'tema_name'),)
 
 
 class UserData(models.Model):
@@ -40,17 +54,9 @@ class UserData(models.Model):
     user_name = models.CharField(max_length=50)
     user_admin = models.CharField(max_length=1, blank=True, null=True)
     login_state = models.CharField(max_length=1, blank=True, null=True)
+    user_email = models.CharField(max_length=512)
+    user_comment = models.CharField(max_length=1024, blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = 'user_data'
-
-
-class UserTeam(models.Model):
-    user = models.OneToOneField(UserData, models.DO_NOTHING, primary_key=True)
-    team_name = models.ForeignKey(TeamData, models.DO_NOTHING, db_column='team_name')
-
-    class Meta:
-        managed = False
-        db_table = 'user_team'
-        unique_together = (('user', 'team_name'),)
