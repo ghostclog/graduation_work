c904958c92c584aeb6e54e20d77e9cca1361bda9
ghostclog <kdy11281@gmail.com>
2023-01-10 15:54:56 +0900

    url 구조를 기존보다 보기 쉽겨 변경 및 마이페이지 진입시 필요한 url 추가

--- the_project/web_back/urls.py
+++ the_project/web_back/urls.py
@@ -3,9 +3,15 @@ from . import views
 
 
 urlpatterns = [
+    #회원가입 관련
     path('regist/', views.user_regist.as_view() ,name='regist'),
     path('id_chk/', views.id_chk.as_view() ,name='id_chk'),
     path('login/', views.user_login.as_view() ,name='login'),
+
+    #마이페이지 관련
+    path('mypage/', views.into_mypage.as_view() ,name='mypage'),
     path('name_ch/', views.name_change.as_view() ,name='name_change'),
     path('pass_ch/', views.pass_change.as_view() ,name='name_change'),
+    path('email_ch/', views.email_change.as_view() ,name='name_change'),
+    
 ]
